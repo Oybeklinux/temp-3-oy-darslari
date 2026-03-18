8373c062830c0e09ca93629e95d7851b2e0d0cc3
Oybek <Oybeklinux@gmail.com>
2026-03-18 09:42:31 +0500

    4-dars. List va tuple

--- 2-dars/1.py
+++ 2-dars/1.py
@@ -1,1 +1,13 @@
-print(1)
+t = (2,3,4)
+
+lst = []
+
+for son in t:
+    if son > 0:
+        lst.append(son)
+
+t = tuple(lst)
+print(t)
+
+
+
--- 3-dars/1.py
+++ 3-dars/1.py
@@ -1,1 +1,9 @@
-print("3-dars")
+n = 5
+
+satr = str(n)
+for i in range(n-1, 0, -1):
+    print(" ", end="")
+    print(" " * (i-1)*2, satr)
+    satr = f"{i} {satr} {i}"
+    
+print(satr)
